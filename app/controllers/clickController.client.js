2bc6138bc5f5119df8d8ef924d323a88039e2c33
Bradley Stachurski <bradley.stachurski@gmail.com>
2016-09-29 21:24:16 +0300

    Trying another APP_URL process.env

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -4,7 +4,7 @@
     var addButton = document.querySelector('.btn-add');
     var deleteButton = document.querySelector('.btn-delete');
     var clickNbr = document.querySelector('#click-nbr');
-    var apiUrl =process.env.APP_URL +'/api/clicks' || 'http://localhost:3000/api/clicks';
+    var apiUrl = process.env.APP_URL + '/api/clicks' || 'http://localhost:3000/api/clicks';
 
     function ready (fn) {
         if (typeof fn !== 'function') {
